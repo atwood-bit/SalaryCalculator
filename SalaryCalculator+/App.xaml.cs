b0eff583a74f917495f30e24b706eba4671b5e70
Artsem Zinko <artsem.zinko-c@therapybrands.com>
2023-02-05 18:47:18 +0300

    Add project files.

---
+++ SalaryCalculator+/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SalaryCalculator_;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ SalaryCalculator+/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SalaryCalculator_;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
